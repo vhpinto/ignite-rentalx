0019be9b52ee023f87b4fd7fb09fbb7bfc0f5598
Vitor Hugo <vhpinto1@gmail.com>
2022-05-15 16:46:46 -0300

    Recebe as categorias por aqruivo e as salva no array

--- src/modules/cars/repositories/ICategoriesRepository.ts
+++ src/modules/cars/repositories/ICategoriesRepository.ts
@@ -9,7 +9,6 @@ interface ICategoriesRepository {
     create({ description, name }: ICreateCategoryDTO): void;
     list(): Category[];
     findByName(name: string) : Category;
-    import(file: Express.Multer.File) : void;
 }
 
 export { ICategoriesRepository, ICreateCategoryDTO };
--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -3,19 +3,53 @@ import { parse } from "csv-parse";
 
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
+interface IImportCategory{
+    name: string;
+    description: string;
+}
+
 class ImportCategoryUseCase {
     constructor(private categoriesRepository: ICategoriesRepository) {
 
     }
-    execute( file : Express.Multer.File): void {
-        const stream = fs.createReadStream(file.path);
-        const parseFile = parse();
 
-        stream.pipe(parseFile);
-
-        parseFile.on("data",async (line) => {
-            console.log(line);
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]>{
+        return new Promise((resolve, reject) => {
+            const categories: IImportCategory[] = [];
+            const stream = fs.createReadStream(file.path);
+            const parseFile = parse();
+        
+            stream.pipe(parseFile);
+        
+            parseFile.on("data", async (line) => {
+                const [name, description] = line;
+                categories.push({
+                    name,
+                    description
+                });
+            }).on("end", () => {
+                resolve(categories);
+            }).on("error", (err) => {
+                reject(err);
+            });
         });
+    }
+
+    async execute( file : Express.Multer.File): Promise<void> {
+        const categories = await this.loadCategories(file);
+
+        categories.map(category => {
+            const { name, description } = category;
+
+            const existCategory = this.categoriesRepository.findByName(name);
+
+            if(!existCategory){
+                this.categoriesRepository.create({
+                    name,
+                    description
+                });
+            }
+        })
     }
 }
 
